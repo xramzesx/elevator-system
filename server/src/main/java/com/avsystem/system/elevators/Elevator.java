e533f3bdf29f4eb33a47a9079190a801c97dd5e6
xramzesx <xramzesbusiness@gmail.com>
2023-07-08 00:36:59 +0200

    [server] add Elevator abstract class

---
+++ server/src/main/java/com/avsystem/system/elevators/Elevator.java
@@ -0,0 +1,56 @@
+package com.avsystem.system.elevators;
+
+import com.avsystem.enums.ElevatorDirection;
+import com.avsystem.records.ElevatorStatus;
+
+/**
+ * Elevator is the abstract base class for each individual elevator.
+ */
+public abstract class Elevator {
+
+    //// ATTRIBUTES ////
+
+    protected final Integer elevatorId;
+    protected Integer currentFloor = 0;
+    protected Integer destinationFloor = 0;
+
+    //// CONSTRUCTOR ////
+
+    public Elevator (Integer elevatorId) {
+        this.elevatorId = elevatorId;
+    }
+
+    //// ABSTRACT METHODS ////
+
+    public abstract void step();
+
+    public abstract void pickup(Integer floor, ElevatorDirection direction);
+
+    //// COMMON METHODS ////
+
+    public void pickup(Integer floor) {
+        this.pickup(
+            floor,
+            ElevatorDirection.get(
+                this.currentFloor,
+                floor
+            )
+        );
+    }
+
+    public ElevatorStatus status() {
+        return new ElevatorStatus(
+            this.elevatorId,
+            this.currentFloor,
+            this.destinationFloor
+        );
+    }
+
+    public Integer distance(Integer floor) {
+        return Math.abs(currentFloor - floor);
+    }
+
+    public Boolean isOpen() {
+        return this.currentFloor.equals(this.destinationFloor);
+    }
+}
