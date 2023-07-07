e096dcb05f6b6a2cf88c3f368cffd092101833b8
xramzesx <xramzesbusiness@gmail.com>
2023-07-08 00:35:15 +0200

    [server] rename ElevatorDirectionTest

--- server/src/test/java/com/avsystem/enums/ElevatorDirectionTests.java
+++ server/src/test/java/com/avsystem/enums/ElevatorDirectionTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ElevatorDirectionTest {
+public class ElevatorDirectionTests {
     @Test
     void getValueTest() {
         assertEquals(-1, ElevatorDirection.DOWN.getValue());
