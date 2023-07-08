2c4840cc43b40123541373383218d72bcab043d7
xramzesx <xramzesbusiness@gmail.com>
2023-07-09 00:24:02 +0200

    [server] add BackSCANState

---
+++ server/src/main/java/com/avsystem/system/elevators/scan/states/queue/BackSCANState.java
@@ -0,0 +1,29 @@
+package com.avsystem.system.elevators.scan.states.queue;
+
+import com.avsystem.enums.ElevatorDirection;
+import com.avsystem.interfaces.State;
+import com.avsystem.system.elevators.scan.SCANElevator;
+
+public class BackSCANState extends QueueSCANState {
+
+    public BackSCANState(SCANElevator elevator, ElevatorDirection direction) {
+        super(elevator, direction);
+    }
+
+    @Override
+    public State enter() {
+        System.out.println(direction + ": Back reached");
+
+        /// TODO: check if opposed ElevatorDirection affects on algorithm
+        this.backwardRequests.add(forwardRequests.pollFirst());
+
+        return this.step();
+    }
+
+    @Override
+    public State step() {
+        State state = new EndSCANState(elevator, direction);
+        return state.enter();
+    }
+
+}
