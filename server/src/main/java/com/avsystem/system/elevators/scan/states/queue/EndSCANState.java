4b51f0f2241ff12f98fc18895fb97703aff1567e
xramzesx <xramzesbusiness@gmail.com>
2023-07-09 00:23:41 +0200

    [server] add EndSCANState

---
+++ server/src/main/java/com/avsystem/system/elevators/scan/states/queue/EndSCANState.java
@@ -0,0 +1,27 @@
+package com.avsystem.system.elevators.scan.states.queue;
+
+import com.avsystem.enums.ElevatorDirection;
+import com.avsystem.interfaces.State;
+import com.avsystem.system.elevators.scan.SCANElevator;
+
+public class EndSCANState extends QueueSCANState {
+
+    public EndSCANState(SCANElevator elevator, ElevatorDirection direction) {
+        super(elevator, direction);
+    }
+
+    @Override
+    public State enter() {
+        System.out.println(direction.toString() + ": End reached");
+
+        this.forwardRequests.addAll(this.postponedRequests);
+        this.postponedRequests.clear();
+
+        return this;
+    }
+
+    @Override
+    public State step() {
+        return this;
+    }
+}
