0a6be5d59dc90a4f1bf3779d8b5de49a6f16a4db
xramzesx <xramzesbusiness@gmail.com>
2023-07-10 15:42:11 +0200

    [server] add ElevatorPickupStrategy

---
+++ server/src/main/java/com/avsystem/system/pickups/ElevatorPickupStrategy.java
@@ -0,0 +1,16 @@
+package com.avsystem.system.pickups;
+
+import com.avsystem.enums.ElevatorDirection;
+import com.avsystem.system.elevators.Elevator;
+
+import java.util.List;
+
+public abstract class ElevatorPickupStrategy {
+    protected List<Elevator> elevators;
+
+    public abstract void pickup (Integer floor, ElevatorDirection direction);
+    public abstract void update(Integer elevatorId, Integer startFloor, Integer finalFloor);
+    public void setContext(List<Elevator> elevators) {
+        this.elevators = elevators;
+    }
+}
