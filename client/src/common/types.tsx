aca9f557da2cc991a36d63caf330e33047420390
xramzesx <xramzesbusiness@gmail.com>
2023-07-12 13:08:04 +0200

    [client] add SimulationConfig type

--- client/src/common/types.tsx
+++ client/src/common/types.tsx
@@ -1,8 +1,29 @@
+//// ENUMS ////
+
+export enum ElevatorSystemVariant {
+    CUSTOM = "CUSTOM",
+    SCAN = "SCAN"
+}
+
+export enum ElevatorFactoryVariant {
+    DEFAULT = "DEFAULT",
+    SCAN = "SCAN",
+    FCFS = "FCFS"
+}
+
+export enum ElevatorPickupStrategy {
+    DEFAULT = "DEFAULT",
+    BCOST = "BCOST"
+}
+
+
 export enum ElevatorDirection {
     UP = "UP",
     DOWN = "DOWN",
     IDLE = "IDLE"
 }
+
+//// TYPES ////
 
 export type ElevatorRequest = {
     floor: number,
@@ -14,4 +35,14 @@ export type ElevatorStatus = {
     current: number,
     destination: number,
     isOpen: boolean
+}
+
+export type SimulationConfig = {
+    systemVariant: ElevatorSystemVariant,
+    factoryVariant: ElevatorFactoryVariant,
+    strategyVariant: ElevatorPickupStrategy,
+    highestFloor: number,
+    lowestFloor: number,
+    elevatorCount: number,
+    delay: number,
 }
