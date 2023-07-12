61f999db8309bae685edc88aa11c5a95436117d0
xramzesx <xramzesbusiness@gmail.com>
2023-07-12 15:33:23 +0200

    [client] move SystemForm to pages/new

---
+++ client/src/pages/new/SystemForm.tsx
@@ -0,0 +1,197 @@
+import { Button, FormControl, MenuItem, Paper, Select, SelectChangeEvent, TextField } from "@mui/material"
+
+import styled from "styled-components"
+import constants from "src/common/constants"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { ElevatorFactoryVariant, ElevatorPickupStrategy, ElevatorSystemVariant, SimulationConfig } from "src/common/types"
+import { useNavigate } from "react-router-dom"
+
+
+const Form = styled.form`
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    align-items: flex-end;
+`
+
+const FieldWrapper = styled.div`
+    display: flex;
+    flex:1;
+    width: 100%;
+    gap: 1rem;
+
+    @media(max-width: ${constants.ui.mobileWidth}) {
+        flex-direction: column;
+    }
+`
+
+const Field = styled.fieldset<{$border?: boolean}>`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    padding: 1rem;
+    border-radius: .5rem;
+    ${props => props.$border ? "" : "border: none;"}
+`
+
+const FieldLegend = styled.legend`
+    font-weight: 500;
+    font-size: 1.5rem;
+    padding-bottom: 1rem;
+    padding: .5rem;
+`
+
+const SystemForm = () => {
+    const navigate = useNavigate()
+
+    const [system, setSystem] = useState(ElevatorSystemVariant.SCAN)
+    const [factory, setFactory] = useState(ElevatorFactoryVariant.DEFAULT)
+    const [strategy, setStrategy] = useState(ElevatorPickupStrategy.DEFAULT)
+    
+    const [elevatorCount, setElevatorCount] = useState(1)
+    const [highestFloor, setHighestFloor] = useState(0)
+    const [lowestFloor, setLowestFloor] = useState(0)
+    const [delay, setDelay] = useState(100)
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleSystemChange = (event: SelectChangeEvent<ElevatorSystemVariant>) => {
+        setSystem(event.target.value as ElevatorSystemVariant)
+    }
+    
+    const handleFactoryChange = (event: SelectChangeEvent<ElevatorFactoryVariant>) => {
+        setFactory(event.target.value as ElevatorFactoryVariant)
+    }
+    
+    const handleStrategyChange = (event: SelectChangeEvent<ElevatorPickupStrategy>) => {
+        setStrategy(event.target.value as ElevatorPickupStrategy)
+    }
+
+   
+    const handleElevatorCountChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const elevators = event.target.value as unknown as number
+
+        if (elevators >= constants.elevator.count.min || elevators <= constants.elevator.count.max)
+            setElevatorCount(elevators)
+    }
+   
+    const handleHighestFloorChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const floor = event.target.value as unknown as number
+                
+        setHighestFloor(floor)
+    }
+   
+    const handleLowestFloorChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const floor = event.target.value as unknown as number
+        setLowestFloor(floor)
+    }
+
+    const handleDelayChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const delay = event.target.value as unknown as number
+        setDelay(delay)
+    }
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        
+        const config : SimulationConfig = {
+            systemVariant : system,
+            factoryVariant: factory,
+            strategyVariant: strategy,
+            highestFloor,
+            lowestFloor,
+            elevatorCount,
+            delay
+        }
+        setIsLoading(true)
+        const response = await fetch(
+            new URL("/simulation", constants.api.url), {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body : JSON.stringify(config)    
+            })
+        
+        setIsLoading(false)
+
+        if (!response.ok) {
+            
+        }
+
+        const { simulationId } = await response.json()
+        
+        navigate(`/simulation/${simulationId}`)
+    }
+
+    return (
+        <Paper>
+            <Form onSubmit={handleSubmit}>
+                <FieldWrapper>
+                    <Field>
+                        <FieldLegend>General</FieldLegend>
+                        <Select
+                            value={system}
+                            onChange={handleSystemChange}
+                        >
+                            <MenuItem value={ElevatorSystemVariant.SCAN}>SCAN System</MenuItem>
+                            <MenuItem value={ElevatorSystemVariant.CUSTOM}>Custom System</MenuItem>
+                        </Select>
+                        <TextField 
+                            value={elevatorCount} 
+                            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} 
+                            label="Elevators" 
+                            onChange={handleElevatorCountChange}
+                        />
+                        <TextField 
+                            value={delay} 
+                            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} 
+                            label="Delay" 
+                            onChange={handleDelayChange}
+                        />
+                        <TextField 
+                            value={highestFloor} 
+                            inputProps={{ inputMode: 'numeric', pattern: '-?[0-9]*' }} 
+                            label="Highest floor" 
+                            onChange={handleHighestFloorChange}
+                        />
+                        <TextField 
+                            value={lowestFloor} 
+                            inputProps={{ inputMode: 'numeric', pattern: '-?[0-9]*' }} 
+                            label="Lowest floor" 
+                            onChange={handleLowestFloorChange}
+                        />
+                    </Field>
+                    <Field $border>
+                        <FieldLegend>System settings</FieldLegend>
+                        <Select
+                            value={factory}
+                            onChange={handleFactoryChange}
+                            disabled={system !== ElevatorSystemVariant.CUSTOM}
+                        >
+                            <MenuItem value={ElevatorFactoryVariant.DEFAULT}>Default factory</MenuItem>
+                            <MenuItem value={ElevatorFactoryVariant.SCAN}>SCAN factory</MenuItem>
+                            <MenuItem value={ElevatorFactoryVariant.FCFS}>FCFS factory</MenuItem>
+                        </Select>
+                        <Select
+                            value={strategy}
+                            onChange={handleStrategyChange}
+                            disabled={system !== ElevatorSystemVariant.CUSTOM}
+                        >
+                            <MenuItem value={ElevatorPickupStrategy.DEFAULT}>Default strategy</MenuItem>
+                            <MenuItem value={ElevatorPickupStrategy.BCOST}>Best Cost strategy</MenuItem>
+                        </Select>
+                    </Field>
+                </FieldWrapper>
+                <Button type="submit" variant="contained" disabled={isLoading}>
+                    Create    
+                </Button>                
+            </Form>
+        </Paper>
+    )
+}
+
+
+export default SystemForm
