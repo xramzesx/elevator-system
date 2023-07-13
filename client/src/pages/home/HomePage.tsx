ae2fe1a2863725184fa924c0cc6113bfc327af12
xramzesx <xramzesbusiness@gmail.com>
2023-07-13 13:11:58 +0200

    [client] add initial HomePage state

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -3,6 +3,9 @@ import useSimulations from "src/hooks/useSimulations"
 import { styled } from "styled-components"
 import SessionsTable from "./SessionsTable"
 import constants from "src/common/constants"
+import { useMemo } from "react"
+import { Button } from "@mui/material"
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
     display: flex;
@@ -22,17 +25,29 @@ const ContentWrapper = styled.div`
 const HomePage = (props: any) => {
     const {data, isLoading, isError} = useSimulations()
     console.log('simulations', data)
+    
+    const content = useMemo(() => !(data ?? new Map<string, SimulationConfig>()).size
+        ? (
+            <Button variant="contained" component={Link} to="/new">
+                Create new simulation
+            </Button>
+        ) : (
+            <ContentWrapper>
+                <SessionsTable
+                    configs={data ?? new Map<string, SimulationConfig>()}
+                />
+            </ContentWrapper>
+        ),[
+        data
+    ])
+
     return (
         <Container>
             <h1>Hi!</h1>
             <p>
                 Create new simulation or choose one from list below!
             </p>
-            <ContentWrapper>
-                <SessionsTable
-                    configs={data ?? new Map<string, SimulationConfig>()}
-                />
-            </ContentWrapper>
+            {content}
         </Container>
     )
 }
