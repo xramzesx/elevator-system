c1339d8c74b747b0e19b35016dc00643eff3b1de
xramzesx <xramzesbusiness@gmail.com>
2023-07-13 21:34:04 +0200

    [server] disable docker build image removal

--- server/build.sh
+++ server/build.sh
@@ -7,4 +7,4 @@ fi
 docker build -t build-elevator-server-jar-image -f Dockerfile.build .
 docker create -it --name build-elevator-server-jar build-elevator-server-jar-image bash
 docker cp build-elevator-server-jar:/target ./target
-docker rm -f build-elevator-server-jar
+# docker rm -f build-elevator-server-jar
