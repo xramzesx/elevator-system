007523f28559c1e27e8c8461f4918c116697f9e3
xramzesx <xramzesbusiness@gmail.com>
2023-07-13 13:06:07 +0200

    [server] add ElevatorFactoryVariant.get

--- server/src/main/java/com/avsystem/enums/ElevatorFactoryVariant.java
+++ server/src/main/java/com/avsystem/enums/ElevatorFactoryVariant.java
@@ -1,5 +1,16 @@
 package com.avsystem.enums;
 
+import com.avsystem.system.factories.ElevatorFactory;
+import com.avsystem.system.factories.FCFSElevatorFactory;
+import com.avsystem.system.factories.SCANElevatorFactory;
+
 public enum ElevatorFactoryVariant {
-    DEFAULT, SCAN, FCFS
+    DEFAULT, SCAN, FCFS;
+
+    public ElevatorFactory get(Integer elevatorCount) {
+        return switch (this) {
+            case DEFAULT, SCAN -> new SCANElevatorFactory(elevatorCount);
+            case FCFS -> new FCFSElevatorFactory(elevatorCount);
+        };
+    }
 }
