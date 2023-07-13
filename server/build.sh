825eaf062c7505637062e887402ffccd499d6ef3
xramzesx <xramzesbusiness@gmail.com>
2023-07-13 13:16:33 +0200

    [server] add bash build script

---
+++ server/build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ -d "./target/" ]
+then
+	rm -r target/
+fi
+docker build -t build-elevator-server-jar-image -f Dockerfile.build .
+docker create -it --name build-elevator-server-jar build-elevator-server-jar-image bash
+docker cp build-elevator-server-jar:/target ./target
+docker rm -f build-elevator-server-jar
